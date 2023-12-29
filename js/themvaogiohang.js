040a5d4828cb8cbe039851f26dfa41e83b7d9943
unknown <24a4041443@bav.edu.vn>
2023-12-30 00:16:07 +0700

    update

--- js/themvaogiohang.js
+++ js/themvaogiohang.js
@@ -1,7 +1,7 @@
 var addProducts = document.getElementsByClassName("themvaogiohang");
     for(var i = 0; i < addProducts.length; i ++){
         addProducts[i].addEventListener('click', function(){
-            alert("Đã thêm sản phẩm vào giỏ hàng");
+            alert("Đã thêm sản phẩm vào giỏ hàng!");
         })
     }
     
